c1ab3ead22c1cae6ab376fb328d1a2036d8360d5
patryk.krawczyk <capybaracreations@gmail.com>
2016-08-10 08:43:12 +0200

    java project init

---
+++ src/main/java/com/patrykkrawczyk/tdddpexample/Main.java
@@ -0,0 +1,10 @@
+package com.patrykkrawczyk.tdddpexample;
+
+/**
+ * Created by Patryk Krawczyk on 10.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
